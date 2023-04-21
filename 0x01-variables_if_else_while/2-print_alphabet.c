1a1b422cc3dfa06ef2525e9da5bd28783d654541
AyaMagdy27 <ayamado1996@gmail.com>
2023-04-21 14:11:25 -0700

    1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -7,12 +7,12 @@
 int main(void)
 {
 	char c = 'a';
-
+	
 	while (c <= 'z')
 	{
-            putchar(c);
-	    c++;
+		putchar(c);
+		c++;
 	}
-        putchar('\n');
+	putchar('\n');
 	return (0);
 }
