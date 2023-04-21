5662285c407b613aad1aa705a555f0070f8bfc42
AyaMagdy27 <ayamado1996@gmail.com>
2023-04-21 11:59:01 -0700

    if else 2

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,3 +1,21 @@
 #include <stdio.h>
 /**
  * main- Print numbers from 0 to 9 and letters from a to f
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i = 0;
+	char c = 'a';
+	while(i < 10){
+	  putchar(i);
+	  i++;
+	}
+	while(c <= f){
+	  putchar(c);
+	  c++;
+	}
+	putchar('\n');
+	return (0);
+}
