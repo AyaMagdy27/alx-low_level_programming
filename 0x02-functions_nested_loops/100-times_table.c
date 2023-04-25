46b6d0bfee5ce53db4cfde2cb459ab454015e645
AyaMagdy27 <ayamado1996@gmail.com>
2023-04-25 11:55:15 -0700

    updated9

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -12,10 +12,11 @@ void print_times_table(int n)
 		for (i = 0; i <= n; i++)
 		{
 			_putchar(48);
-			for (j = 0; j <= n; j++)
+			for (j = 1; j <= n; j++)
 			{
 				k = j * i;
 				_putchar(',');
+				_putchar(' ');
 				if (k <= 9)
 					_putchar(' ');
 				if (k <= 99)
