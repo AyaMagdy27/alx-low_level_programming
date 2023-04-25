672d2dac1c504010f9ca94036550800bf4afc736
AyaMagdy27 <ayamado1996@gmail.com>
2023-04-25 09:38:28 -0700

    C - Functions, nested loops updated6

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
  * print_alphabet_x10 -  prints 10 times the alphabet in lowercase
- * Return: Always 0 (Success)
  */
 void print_alphabet_x10(void)
 {
@@ -16,5 +15,4 @@ void print_alphabet_x10(void)
 		}
 		_putchar('\n');
 	}
-	return (0);
 }
