e5c645cccd2cdfbec053cbab0f5f41f2de231f9b
AyaMagdy27 <ayamado1996@gmail.com>
2023-05-04 11:50:35 -0700

    adv 1

---
+++ 0x06-pointers_arrays_strings/100-rot13.c
@@ -0,0 +1,25 @@
+#include "main.h"
+/**
+ * char *rot13- encodes a string using rot13
+ * @str: pointer to string params
+ * Return: *s
+ */
+char *rot13(char *str)
+{
+	int i, j;
+	char input[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+	char output[] = "NOPQRSTUVWXYZABCDEFGHIJKLMnopqrstuvwxyzabcdefghijklm";
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		for (j = 0; j < 52; j++)
+		{
+			if (str[i] == input[j])
+			{
+				str[i] = output[j];
+				break;
+			}
+		}
+	}
+	return (str);
+}
