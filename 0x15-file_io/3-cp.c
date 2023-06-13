fa546ba3c85996a2529ea7d223b5eeafe0df6c00
AyaMagdy27 <ayamado1996@gmail.com>
2023-06-12 21:34:23 -0700

    main 3 update

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -33,7 +33,7 @@ void close_file(int fd)
 	c = close(fd);
 	if (c == -1)
 	{
-		dprintf(STDERR_FILENO, "Error:can't close fd %d\n", fd);
+		dprintf(STDERR_FILENO, "Error: Can't close fd\n");
 		exit(100);
 	}
 }
@@ -86,10 +86,26 @@ int main(int argc, char *argv[])
 		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-	} while (r > 0);
-
-	free(buffer);
-	close_file(from);
-	close_file(to);
-	return (0);
+		do {
+			if (from == -1 || r == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+				free(buffer);
+				exit(98);
+			}
+			w = write(to, buffer, r);
+			if (to == -1 || w == -1)
+			{
+				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+				free(buffer);
+				exit(99);
+			}
+			r = read(from, buffer, 1024);
+		}while (r > 0)
+		{
+		free(buffer);
+		close_file(from);
+		close_file(to);
+		return (0);
+		}
 }
