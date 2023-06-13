e32c1c6e86a3d64279599f4604eb5501752062dd
AyaMagdy27 <ayamado1996@gmail.com>
2023-06-13 04:24:44 -0700

    main3

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -68,12 +68,10 @@ int main(int argc, char *argv[])
 	from = open(argv[1], O_RDONLY);
 	r = read(from, buffer, 1024);
 	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-
 	do {
 		if (from == -1 || r == -1)
 		{
-			dprintf(STDERR_FILENO,
-					"Error: Can't read from file %s\n", argv[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			free(buffer);
 			exit(98);
 		}
@@ -86,26 +84,9 @@ int main(int argc, char *argv[])
 		}
 		r = read(from, buffer, 1024);
 		to = open(argv[2], O_WRONLY | O_APPEND);
-		do {
-			if (from == -1 || r == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-				free(buffer);
-				exit(98);
-			}
-			w = write(to, buffer, r);
-			if (to == -1 || w == -1)
-			{
-				dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-				free(buffer);
-				exit(99);
-			}
-			r = read(from, buffer, 1024);
-		}while (r > 0)
-		{
-		free(buffer);
-		close_file(from);
-		close_file(to);
-		return (0);
-		}
+	} while (r > 0);
+	free(buffer);
+	close_file(from);
+	close_file(to);
+	return (0);
 }
