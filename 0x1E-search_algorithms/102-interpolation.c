817384a294be07d7c381db7c5dbe0481a801c0ed
AyaMagdy27 <ayamado1996@gmail.com>
2024-05-21 17:28:38 -0700

    fix

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -20,8 +20,7 @@ int interpolation_search(int *array, size_t size, int value)
 	while (array[low] != array[high] && value >= array[low] &&
 	       value <= array[high])
 	{
-		mid = low + (((double) (high - low) / (array[high] - array[low])) *
-		             (value - array[low]));
+		mid = low + (((double) (high - low) / (array[high] - array[low])) * (value - array[low]));
 
 		printf("Value checked array[%ld] = [%d]\n", mid, array[mid]);
 		if (value > array[mid])
@@ -31,8 +30,7 @@ int interpolation_search(int *array, size_t size, int value)
 		else
 			return (mid);
 	}
-	mid = low + (((double) (high - low) / (array[high] - array[low])) *
-	             (value - array[low]));
+	mid = low + (((double) (high - low) / (array[high] - array[low])) * (value - array[low]));
 	printf("Value checked array[%ld] is out of range\n", mid);
 	return (-1);
 }
